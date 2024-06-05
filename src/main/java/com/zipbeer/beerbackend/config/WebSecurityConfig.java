fac1e17beee0df3ec5b67c9aa9fbb05ef2258af6
YangShin <g56261822@gmail.com>
2024-06-05 15:14:32 +0900

    kys

--- src/main/java/com/zipbeer/beerbackend/config/WebSecurityConfig.java
+++ src/main/java/com/zipbeer/beerbackend/config/WebSecurityConfig.java
@@ -48,7 +48,6 @@ public class WebSecurityConfig {
     private final CustomSuccessHandler customSuccessHandler;
     private final CustomLogoutFilter customLogoutFilter;
 
-    //AuthenticationManager Bean 등록
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
@@ -59,49 +58,29 @@ public class WebSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
-//        loginFilter.setFilterProcessesUrl(""); 지정 시 post 요청 url 변경 가능
-
         http
-                .cors(cors -> cors
-                        .configurationSource(corsConfigurationSource()))
-                //formLogin 방식은 disable 해서 usernamePasswordAuthenticationFilter 직접 등록
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .formLogin(AbstractHttpConfigurer::disable)
-
                 .csrf(CsrfConfigurer::disable)
-
                 .httpBasic(HttpBasicConfigurer::disable)
-
-                .sessionManagement(sessionManagement -> sessionManagement
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .oauth2Login(oauth2 -> {
-                    oauth2.userInfoEndpoint(userInfoEndpointConfig -> {
-                                userInfoEndpointConfig.userService(customOAuth2UserService);
-                            })
+                    oauth2.userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig.userService(customOAuth2UserService))
                             .successHandler(customSuccessHandler);
                 })
-
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/", "/login", "/reissue","/api/v1/auth/**", "/api/camp/**", "/api/weather/**", "/api/notice/**").permitAll()
-                        .requestMatchers(HttpMethod.GET,"/api/review/**").permitAll()
-                        .requestMatchers(HttpMethod.GET,"/api/user/**").permitAll()
+                        .requestMatchers("/", "/login", "/reissue", "/api/v1/auth/**", "/api/camp/**", "/api/weather/**", "/api/notice/**", "/ws/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/review/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/user/**").permitAll()
                         .requestMatchers("/api/v1/user/**").hasRole("USER")
                         .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
-
-                .exceptionHandling(exceptionHandling -> exceptionHandling
-                        .authenticationEntryPoint(new FailedAuthenticationEntryPoint()))
-
-                //로그인 필터 이전에 jwt 인증 필터 등록
+                .exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(new FailedAuthenticationEntryPoint()))
                 .addFilterBefore(jwtAuthenticationFilter, LoginFilter.class)
                 .addFilterBefore(customLogoutFilter, LogoutFilter.class)
-
-                //로그인 필터를 usernamePasswordAuthenticationFilter 로 등록
                 .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtProvider), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
@@ -115,18 +94,15 @@ public class WebSecurityConfig {
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
         corsConfiguration.setMaxAge(3600L);
-
         corsConfiguration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
         corsConfiguration.setExposedHeaders(Collections.singletonList("Authorization"));
         corsConfiguration.setExposedHeaders(Collections.singletonList("access"));
-
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
 
         return source;
     }
-
 }
 
 class FailedAuthenticationEntryPoint implements AuthenticationEntryPoint {
@@ -137,7 +113,5 @@ class FailedAuthenticationEntryPoint implements AuthenticationEntryPoint {
         response.setContentType("application/json");
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
         response.getWriter().write("{\"code\" : \"NP\", \"message\" : \"No Permission\"}");
-        // {"code" : "NP", "message" : "No Permission"}
     }
-
 }
