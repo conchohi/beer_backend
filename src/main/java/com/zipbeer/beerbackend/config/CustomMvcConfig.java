4ee8ac70b42d9a921da77e3e83ebb611af3803fc
conchohi <lar3242@gmail.com>
2024-06-12 15:58:34 +0900

    개발환경

--- src/main/java/com/zipbeer/beerbackend/config/CustomMvcConfig.java
+++ src/main/java/com/zipbeer/beerbackend/config/CustomMvcConfig.java
@@ -15,7 +15,7 @@ public class CustomMvcConfig implements WebMvcConfigurer {
                 .allowedMethods("*")
                 .exposedHeaders("Set-Cookie","Content-Type")
                 .allowCredentials(true)
-                .allowedOrigins("https://js1.jsflux.co.kr");
+                .allowedOrigins("http://localhost:3000");
     }
 
     @Override
