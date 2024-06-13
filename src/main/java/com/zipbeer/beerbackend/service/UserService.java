e91695f0fa95b5b4114e20f15de3ed21497560da
conchohi <lar3242@gmail.com>
2024-06-13 22:11:07 +0900

    불필요한 파일 삭제

--- src/main/java/com/zipbeer/beerbackend/service/UserService.java
+++ src/main/java/com/zipbeer/beerbackend/service/UserService.java
@@ -8,15 +8,10 @@ import java.util.Optional;
 
 public interface UserService {
     UserDto getUserById(String id);
-    Optional<UserDto> updateUserByNickname(String nickname, UserDto userDto);
     void modify(UserDto userDto);
     UserDto getUserByNickname(String nickname);
     //    public void delete(String userId);
     boolean isIdAvailable(String userid);
     boolean isNicknameAvailable(String nickname);
-    Optional<UserDto> updateNickname(String userId, String newNickname); // New method to update nickname
-    List<String> getUserIdsByEmail(String email);
-    boolean emailExists(String email);
-    Optional<UserDto> updateUserProfile(String userId, MultipartFile profileFile);
     List<UserDto> searchUsersByNickname(String nickname);
 }
--- src/main/java/com/zipbeer/beerbackend/service/impl/UserServiceImpl.java
+++ src/main/java/com/zipbeer/beerbackend/service/impl/UserServiceImpl.java
@@ -50,28 +50,6 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public Optional<UserDto> updateUserByNickname(String nickname, UserDto userDto) {
-        Optional<UserEntity> optionalUser = userRepository.findByNickname(nickname);
-        if (optionalUser.isPresent()) {
-            UserEntity user = optionalUser.get();
-            user.setMbti(userDto.getMbti());
-            user.setAge(userDto.getAge());
-            user.setIntro(userDto.getIntro());
-
-            MultipartFile multipartFile = userDto.getProfileFile();
-            if (multipartFile != null && !multipartFile.isEmpty()) {
-                String profileImage = fileUtil.saveFile(multipartFile);
-                fileUtil.deleteFile(user.getProfileImage());
-                user.setProfileImage(profileImage);
-            }
-
-            userRepository.save(user);
-            return Optional.of(new UserDto(user));
-        }
-        return Optional.empty();
-    }
-
-    @Override
     public void modify(UserDto userDto) {
         UserEntity user = wrapUserEntity(userRepository.findByUserId(userDto.getUserId()))
                 .orElseThrow(() -> new RuntimeException("User not found"));
@@ -104,45 +82,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public boolean isNicknameAvailable(String nickname) {
         return !userRepository.existsByNickname(nickname);
-    }
-
-    @Override
-    public Optional<UserDto> updateNickname(String userId, String newNickname) {
-        Optional<UserEntity> optionalUser = wrapUserEntity(userRepository.findByUserId(userId));
-        if (optionalUser.isPresent()) {
-            UserEntity user = optionalUser.get();
-            if (!userRepository.existsByNickname(newNickname)) {
-                user.setNickname(newNickname);
-                userRepository.save(user);
-                return Optional.of(new UserDto(user));
-            }
-        }
-        return Optional.empty();
-    }
-
-    @Override
-    public List<String> getUserIdsByEmail(String email) {
-        return userRepository.findUserIdsByEmail(email);
-    }
-
-    @Override
-    public boolean emailExists(String email) {
-        return userRepository.existsByEmail(email);
-    }
-
-    @Override
-    public Optional<UserDto> updateUserProfile(String userId, MultipartFile profileFile) {
-        Optional<UserEntity> optionalUser = wrapUserEntity(userRepository.findByUserId(userId));
-        if (optionalUser.isPresent()) {
-            UserEntity user = optionalUser.get();
-            String beforeProfileImage = user.getProfileImage();
-            String profileImage = fileUtil.saveFile(profileFile);
-            user.setProfileImage(profileImage);
-            fileUtil.deleteFile(beforeProfileImage);
-            userRepository.save(user);
-            return Optional.of(new UserDto(user));
-        }
-        return Optional.empty();
     }
 
     // Helper method to wrap UserEntity in Optional
