fca010284a9956fbde7f3297ec9acf44b2aa3158
conchohi <lar3242@gmail.com>
2024-06-13 15:03:45 +0900

    배포 설정

--- src/main/java/com/zipbeer/beerbackend/config/WebSocketConfig.java
+++ src/main/java/com/zipbeer/beerbackend/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:3000")
+                .setAllowedOrigins("https://js1.jsflux.co.kr")
                 .withSockJS();
     }
 }
