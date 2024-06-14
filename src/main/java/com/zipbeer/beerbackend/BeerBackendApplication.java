77a3c78db273b10a6c968ddc77a4fdbd1fad88bc
conchohi <lar3242@gmail.com>
2024-06-14 11:22:36 +0900

    시간대 설정

--- src/main/java/com/zipbeer/beerbackend/BeerBackendApplication.java
+++ src/main/java/com/zipbeer/beerbackend/BeerBackendApplication.java
@@ -1,13 +1,19 @@
 package com.zipbeer.beerbackend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BeerBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BeerBackendApplication.class, args);
     }
-
+    @PostConstruct
+    public void init() {
+        // timezone 설정
+        TimeZone.setDefault(TimeZone.getTimeZone("KST"));
+    }
 }
